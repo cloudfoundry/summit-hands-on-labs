a8fd0a79a68d88dc1e09148868febc244f038f54
Ruben Koster <rkoster@starkandwayne.com>
2018-10-01 19:40:46 +0200

    Switch back to running on mac (since it saves the copy auth code step)

--- basel-2018/shared/login_all.sh
+++ basel-2018/shared/login_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-[ -z "$CLOUD_SHELL" ] && echo "This script needs to run from Google Cloud Shell" && exit 1;
-
 for i in $(seq 12); do
     account=training.hol.$i@cloudfoundry.org
     echo "Login to ${account}"
+    echo ${account} | pbcopy
     gcloud auth login --account=${account} --quiet
 done
 wait
 
-sqlite3 /tmp/*/credentials.db "SELECT value FROM credentials;" ".schema credentials;" ".exit" \
+config_path=$(gcloud info --format json | jq -r '.config.paths.global_config_dir')
+sqlite3 ${config_path}/credentials.db "SELECT value FROM credentials;" ".schema credentials;" ".exit" \
     | jq -c '.id_token | {email: .email, expires: (.exp | todate)}'
