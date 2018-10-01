de22ca44fd518248928e1553fb92cc3ab5fbb0e5
Ruben Koster <rkoster@starkandwayne.com>
2018-10-01 14:42:52 +0200

    Make sure we are using Google Cloud Shell

--- basel-2018/shared/login_all.sh
+++ basel-2018/shared/login_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -z "$CLOUD_SHELL" ] && echo "This script needs to run from Google Cloud Shell" && exit 1;
+
 for i in $(seq 12); do
     gcloud auth login --account=training.hol.$i@cloudfoundry.org --quiet
 done
