1c583bc322f95abf5f8e487953f1ec1957033c54
Dimitar Donchev <dimitar.donchev@sap.com>
2019-04-02 22:43:11 -0400

    multiapps final adjustments

--- philadelphia-2019/multiapps/setup-lab.sh
+++ philadelphia-2019/multiapps/setup-lab.sh
@@ -3,4 +3,5 @@ source ${SCRIPT_DIR}/scripts/wget_plugin_mbt.sh
 source ${SCRIPT_DIR}/scripts/login.sh
 
 (git clone https://github.com/ddonchev/multiapps-handson ${HOME}/mtalab)
-echo "please enter 'cd ${HOME}/mtalab' and follow the instructions on https://github.com/cloudfoundry/summit-hands-on-labs/tree/master/philadelphia-2019/multiapps#table-of-contents"
+cd ${HOME}/mtalab
+(git checkout master ; git reset --hard origin/master)
