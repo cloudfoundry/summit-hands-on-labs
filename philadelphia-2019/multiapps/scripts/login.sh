0de3b60ab6652aedc8ab0263bd43a3bc5f3fb9b0
Dimitar Donchev <dimitar.donchev@sap.com>
2019-04-02 22:43:11 -0400

    multiapps final adjustments

--- philadelphia-2019/multiapps/scripts/login.sh
+++ philadelphia-2019/multiapps/scripts/login.sh
@@ -1,6 +1,6 @@
 echo "Logging you in to Sap Cloud Platform..."
 account=$(gcloud config get-value account)
-space=${account: 7:2 }
+space=${account: 13:2 }
 pass="${account: 0:7 }CFNAS19!"  
 echo "pas: $pass space: $space acc: $account"
 cf login -a https://api.cf.us10.hana.ondemand.com/ -u d.imdonchev@gmail.com -o mta -p ${pass} -s handson
